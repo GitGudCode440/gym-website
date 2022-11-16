e4c1927254417b8ec86c0e43cb99abf6fe7f73a3
M.Mahad Hashmi <m.mahadhashmi@gmail.com>
2022-11-16 14:32:21 +0500

    Added icons to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,16 @@
+import dumbbellLogo from '../assets/dumbbell_logo.svg'
+import menuIcon from '../assets/menu_icon.svg'
+
 function Header() {
   return (
     <header className="header">
       <nav className="header-nav">
-        <a href="">LOGO</a>
-        <a href="">JOIN NOW</a>
+        <a href="#">
+          <img src={dumbbellLogo} alt="A logo of a dumbbell" />
+        </a>
+        <a href="#">
+          <img src={menuIcon} alt="The menu icon" />
+        </a>
       </nav>
     </header>
   )
