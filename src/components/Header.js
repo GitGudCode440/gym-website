b387beca2c9268348d6b02c71997185a2e1cc153
M.Mahad Hashmi <m.mahadhashmi@gmail.com>
2022-11-16 13:35:45 +0500

    Prototyped a header with basic styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,11 @@
 function Header() {
   return (
-    <div>Header</div>
+    <header className="header">
+      <nav className="header-nav">
+        <a href="">LOGO</a>
+        <a href="">JOIN NOW</a>
+      </nav>
+    </header>
   )
 }
 
